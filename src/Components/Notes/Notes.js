5c783605fe8bc7906e104fa729b69b9532a59163
FayedAlMamun <fayedruet@gmail.com>
2021-05-28 01:42:02 +0600

    updated

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -3,27 +3,27 @@ import './Notes.css'
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Display from '../Display/Display';
+import { useFormik } from 'formik';
 const Notes = () => {
-    const [notes, setNotes] = useState({
-        title: "",
-        msg: ""
+    const [show, setShow] = useState(JSON.parse(window.localStorage.getItem('data')));
+    show && window.localStorage.setItem('data',JSON.stringify(show))
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            msg: ''
+        },
+        onSubmit: (values, { resetForm }) => {
+            const data = [...show, values];
+            setShow(data);
+            resetForm({ values: "" })
+        },
+        validate: values => {
+            let errors = {}
+            if (!values.title) errors.title = "Name can ot be Empty!"
+            if (!values.msg) errors.msg = "Please write something!"
+            return errors
+        }
     })
-    const [show, setShow] = useState([]);
-    const eventInput = (e) => {
-        const note = { ...notes };
-        note[e.target.name] = e.target.value;
-        setNotes(note);
-        console.log(notes);
-    }
-    const setClick = (e) => {
-        const data = [...show, notes];
-        setShow(data);
-        setNotes({
-            title: "",
-            msg: ""
-        })
-        e.preventDefault();
-    }
     const [expand, setExpand] = useState(false);
     const showMe = () => {
         setExpand(true)
@@ -34,17 +34,20 @@ const Notes = () => {
         })
         setShow(texts);
     }
-    const againClicked=()=>{
+    const againClicked = () => {
         setExpand(false)
 
     }
     return (<>
         <div className='main d-flex ms-3 justify-content-center'>
             <div className='mt-3 second' onDoubleClick={againClicked}>
-                <form className='m-3' onSubmit={notes.title && notes.msg ? setClick : 'none'}>
-                    {expand && <input onChange={eventInput} value={notes.title} required name='title' type="text" placeholder='Title' />}<br /><br />
-                    <textarea onChange={eventInput} onClick={showMe} value={notes.msg} required name="msg" id="" placeholder='Write a post!'>
+                <form className='m-3' onSubmit={formik.handleSubmit}>
+                    {/* onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.title} replaced by getFieldProps */}
+                    {expand && <input {...formik.getFieldProps('title')} name='title' type="text" placeholder='Title' />}<br /><br />
+                    {formik.errors.title && formik.touched.title ? <div className="error">{formik.errors.title}</div> : null}
+                    <textarea {...formik.getFieldProps('msg')} onClick={showMe} name="msg" id="" placeholder='Write a post!'>
                     </textarea>
+                    {formik.errors.msg && formik.touched.msg ? <div className="error">{formik.errors.msg}</div> : null}
                     {
                         expand &&
                         <div className="d-flex justify-content-end">
@@ -57,7 +60,7 @@ const Notes = () => {
             </div>
         </div>
         <div className="row m-4">
-            {
+            { window.localStorage.getItem('data') &&
                 show.map((e, index) => {
                     return [
                         <Display title={e.title}
